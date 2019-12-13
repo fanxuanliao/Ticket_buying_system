c7b30dcee44ce150fda20bef2a5e79beb54d12cd
Hazel <Hazel@Hazel>
2019-12-13 21:51:57 +0800

    hazel commit

--- Finalproject/src/finalproject/shaishuan.java
+++ Finalproject/src/finalproject/shaishuan.java
@@ -10,6 +10,7 @@ import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.layout.RowData;
 
 public class shaishuan extends Composite {
 
@@ -18,6 +19,7 @@ public class shaishuan extends Composite {
 	 * @param parent
 	 * @param style
 	 */
+	int pa=1, pb=2,pc=3,pd=4,pe=5,pf=6;
 	public shaishuan(Composite parent, int style) {
 		super(parent, style);
 		setLayout(new FormLayout());
@@ -44,12 +46,13 @@ public class shaishuan extends Composite {
 		btnCheckButton_3.setText("Korean");
 		
 		Composite composite_1 = new Composite(this, SWT.NONE);
-		composite_1.setLayout(new RowLayout(SWT.HORIZONTAL));
 		FormData fd_composite_1 = new FormData();
+		fd_composite_1.bottom = new FormAttachment(0, 136);
+		fd_composite_1.right = new FormAttachment(0, 404);
+		fd_composite_1.top = new FormAttachment(0, 90);
 		fd_composite_1.left = new FormAttachment(0, 75);
-		fd_composite_1.bottom = new FormAttachment(100, -579);
-		fd_composite_1.top = new FormAttachment(composite, 6);
 		composite_1.setLayoutData(fd_composite_1);
+		composite_1.setLayout(new RowLayout(SWT.HORIZONTAL));
 		
 		Button button = new Button(composite_1, SWT.CHECK);
 		button.setText("\u9A5A\u609A");
@@ -60,36 +63,111 @@ public class shaishuan extends Composite {
 		Button button_2 = new Button(composite_1, SWT.CHECK);
 		button_2.setText("\u52D5\u4F5C");
 		
-		Label lblNewLabel = new Label(this, SWT.NONE);
-		lblNewLabel.setTouchEnabled(true);
-		FormData fd_lblNewLabel = new FormData();
-		fd_lblNewLabel.bottom = new FormAttachment(composite_1, 439, SWT.BOTTOM);
-		fd_lblNewLabel.top = new FormAttachment(composite_1, 23);
-		fd_lblNewLabel.left = new FormAttachment(composite, 0, SWT.LEFT);
-		fd_lblNewLabel.right = new FormAttachment(0, 473);
-		lblNewLabel.setLayoutData(fd_lblNewLabel);
-		String X = "test\n1111\n";
-		//lblNewLabel.setText(X);
-
+		Button button_3 = new Button(this, SWT.NONE);
+		FormData fd_button_3 = new FormData();
+		fd_button_3.right = new FormAttachment(0, 472);
+		fd_button_3.top = new FormAttachment(0, 185);
+		fd_button_3.left = new FormAttachment(0, 75);
+		button_3.setLayoutData(fd_button_3);
+		
+		Button button_4 = new Button(this, SWT.NONE);
+		FormData fd_button_4 = new FormData();
+		fd_button_4.right = new FormAttachment(0, 472);
+		fd_button_4.top = new FormAttachment(0, 216);
+		fd_button_4.left = new FormAttachment(0, 75);
+		button_4.setLayoutData(fd_button_4);
+		
+		Button button_5 = new Button(this, SWT.NONE);
+		FormData fd_button_5 = new FormData();
+		fd_button_5.right = new FormAttachment(0, 472);
+		fd_button_5.top = new FormAttachment(0, 247);
+		fd_button_5.left = new FormAttachment(0, 75);
+		button_5.setLayoutData(fd_button_5);
+		
+		Button button_6 = new Button(this, SWT.NONE);
+		FormData fd_button_6 = new FormData();
+		fd_button_6.right = new FormAttachment(0, 472);
+		fd_button_6.top = new FormAttachment(0, 278);
+		fd_button_6.left = new FormAttachment(0, 75);
+		button_6.setLayoutData(fd_button_6);
+		
+		Button button_7 = new Button(this, SWT.NONE);
+		FormData fd_button_7 = new FormData();
+		fd_button_7.right = new FormAttachment(0, 472);
+		fd_button_7.top = new FormAttachment(0, 309);
+		fd_button_7.left = new FormAttachment(0, 75);
+		button_7.setLayoutData(fd_button_7);
+		
+		Button button_8 = new Button(this, SWT.NONE);
+		FormData fd_button_8 = new FormData();
+		fd_button_8.right = new FormAttachment(0, 472);
+		fd_button_8.top = new FormAttachment(0, 340);
+		fd_button_8.left = new FormAttachment(0, 75);
+		button_8.setLayoutData(fd_button_8);
 		
 		Button chooseEnd = new Button(this, SWT.NONE);
+		FormData fd_chooseEnd = new FormData();
+		fd_chooseEnd.bottom = new FormAttachment(0, 136);
+		fd_chooseEnd.right = new FormAttachment(0, 522);
+		fd_chooseEnd.top = new FormAttachment(0, 100);
+		fd_chooseEnd.left = new FormAttachment(0, 410);
+		chooseEnd.setLayoutData(fd_chooseEnd);
 		chooseEnd.addSelectionListener(new SelectionAdapter() {
 			@Override
-			public void widgetSelected(SelectionEvent e) {
-				for(int i=0;i<4;i++) {
-					lblNewLabel.setText("XDXDXD\n");					
-				}
+			public void widgetSelected(SelectionEvent e) {		
+					button_3.setText(Integer.toString(pa));
+					button_4.setText(Integer.toString(pb));
+					button_5.setText(Integer.toString(pc));
+					button_6.setText(Integer.toString(pd));
+					button_7.setText(Integer.toString(pe));
+					button_8.setText(Integer.toString(pf));
 
 			}
 		});
-		fd_composite_1.right = new FormAttachment(100, -370);
-		FormData fd_chooseEnd = new FormData();
-		fd_chooseEnd.bottom = new FormAttachment(composite_1, 0, SWT.BOTTOM);
-		fd_chooseEnd.left = new FormAttachment(composite_1, 6);
-		fd_chooseEnd.top = new FormAttachment(0, 100);
-		fd_chooseEnd.right = new FormAttachment(100, -252);
-		chooseEnd.setLayoutData(fd_chooseEnd);
 		chooseEnd.setText("\u9078\u64C7\u7D42\u4E86");
+		
+		Composite composite_2 = new Composite(this, SWT.NONE);
+		FormData fd_composite_2 = new FormData();
+		fd_composite_2.top = new FormAttachment(button_8, 18);
+		fd_composite_2.left = new FormAttachment(chooseEnd, 0, SWT.LEFT);
+		fd_composite_2.bottom = new FormAttachment(0, 447);
+		fd_composite_2.right = new FormAttachment(0, 625);
+		composite_2.setLayoutData(fd_composite_2);
+		composite_2.setLayout(new RowLayout(SWT.HORIZONTAL));
+		
+		Button btnBack = new Button(composite_2, SWT.NONE);
+		btnBack.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if(pa>1) {
+					button_3.setText(Integer.toString((pa-=6)));
+					button_4.setText(Integer.toString((pb-=6)));
+					button_5.setText(Integer.toString((pc-=6)));
+					button_6.setText(Integer.toString((pd-=6)));
+					button_7.setText(Integer.toString((pe-=6)));
+					button_8.setText(Integer.toString((pf-=6)));
+				}
+				else {
+				}
+			}
+		});
+		btnBack.setLayoutData(new RowData(94, SWT.DEFAULT));
+		btnBack.setText("back");
+		
+		Button btnNext = new Button(composite_2, SWT.NONE);
+		btnNext.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				button_3.setText(Integer.toString((pa+=6)));
+				button_4.setText(Integer.toString((pb+=6)));
+				button_5.setText(Integer.toString((pc+=6)));
+				button_6.setText(Integer.toString((pd+=6)));
+				button_7.setText(Integer.toString((pe+=6)));
+				button_8.setText(Integer.toString((pf+=6)));
+			}
+		});
+		btnNext.setLayoutData(new RowData(96, SWT.DEFAULT));
+		btnNext.setText("next");
 		
 		
 
